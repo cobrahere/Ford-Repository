e0d7864d56bf44a186fa0a809f7e25eabf504ec1
Nas Iqbal <maiq@deakin.edu.au>
2020-11-24 14:15:09 +1100

    Hello Wolrd save

---
+++ Hello_World/hello/src/app/my-form/my-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-form',
+  templateUrl: './my-form.component.html',
+  styleUrls: ['./my-form.component.sass']
+})
+export class MyFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
